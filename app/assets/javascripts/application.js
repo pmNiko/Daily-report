16185c321d5b84ae6a7812ad6a8ff9c3f9e87fe7
Nikolas <nikolas090189@gmail.com>
2018-01-12 21:03:26 -0300

    view all

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,10 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require rails-ujs
-//= require turbolinks
+//= require bootstrap-sprockets
+//= require chosen-jquery
 //= require bootstrap-sprockets
 //= require_tree .
+//= require turbolinks
