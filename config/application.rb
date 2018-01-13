2c604346b3460ca15be3e4e8a6fc8e99aa454a3c
Nikolas <nikolas090189@gmail.com>
2018-01-13 20:46:23 -0300

    dont gliphicon

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,7 @@ module DailyReport
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    # Add the fonts path
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  # Precompile additional assets
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
