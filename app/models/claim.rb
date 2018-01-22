4dc76e06a64f59107c77464fe25d020adc18500e
Nikolas <nikolas090189@gmail.com>
2018-01-22 00:28:01 -0300

    View claims index

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -1,7 +1,21 @@
 class Claim < ApplicationRecord
+  #The class Claim has many responsables
   has_and_belongs_to_many :users
-
-  def responsables
+  #Return the responsables
+  def responsibles
     @users = self.users
   end
+  #Return true or false if contain only one responsable
+  def only_one_responsible?
+    self.responsibles.count == 1
+  end
+
+  def responsible_first
+    return self.responsibles.first
+  end
+
+  def responsible_second
+    return self.responsibles.second
+  end
+
 end
